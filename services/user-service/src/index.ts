87a1df69ce40d4876bab1aedc853c3a95f2afdae
bryantjandra <tjandra.benedict@gmail.com>
2026-03-09 20:02:56 +0800

    chore: setup frontend and neccesary services

---
+++ services/user-service/src/index.ts
@@ -0,0 +1,32 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+const PORT = process.env.PORT || 8081;
+const MONGO_URI =
+  process.env.MONGO_URI || "mongodb://localhost:27017/user-service";
+const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS?.split(",") || [
+  "http://localhost:5173",
+];
+
+app.use(cors({ origin: ALLOWED_ORIGINS, credentials: true }));
+app.use(express.json());
+
+async function startServer(): Promise<void> {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("User Service - Connected to MongoDB");
+
+    app.listen(PORT, () => {
+      console.log(`User Service listening on PORT ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+startServer();
